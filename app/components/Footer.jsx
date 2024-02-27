c2d766785bab7ed06e1ca96f9175fd6317b89ca7
Buiscki1 <122836250+Buiscki1@users.noreply.github.com>
2024-02-27 04:58:22 +0100

    Footer and text

---
+++ app/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer fixed bottom-0 right-0 items-center p-4 bg-neutral text-neutral-content hidden lg:block ">
+      <div className="items-center flex-col">
+        <p className="text-slate-300 glitch text-sm">Karli Christensen © 2024</p>
+
+      </div>
+    </footer>
+  );
+};
+export default Footer;
--- app/page.js
+++ app/page.js
@@ -26,9 +26,7 @@ export default function Home() {
         ></svg>
         <div id="main-contents" className="w-[90%] h-full p-5 mt-16">
           <div className="flex flex-row justify-between">
-            <h1 className="glitch text-slate-300 text-3xl ml-0">
-              DIGITAL PORTFOLIO
-            </h1>
+            <h1 className="glitch text-slate-300 text-3xl ml-0">WELCOME</h1>
             <div className="m-0">
               <ul className="flex flex-row">
                 <li className="w-[30px] h-[30px] hover:scale-110 ease-out duration-200 ml-10">
@@ -38,10 +36,7 @@ export default function Home() {
                   </a>
                 </li>
                 <li className="w-[30px] h-[30px] hover:scale-110 ease-out duration-200 ml-10">
-                  <a
-                    href="https://www.linkedin.com/in/karlichristensen/"
-                    className=""
-                  >
+                  <a href="https://www.linkedin.com/in/karlichristensen/">
                     {" "}
                     <Image
                       src={linkedinIcon}
@@ -61,38 +56,57 @@ export default function Home() {
               </ul>
             </div>
           </div>
+          <div className="w-[40%]">
+            <p className="my-10 text-slate-300 min-w-[400px]">
+              Hellio, my name is Karli Christensen, I'm a front-end developer
+              from <span className="glitch">Copenhagen, Denmark.</span> I build
+              and design things. Occasionally those things <span className="glitch"> work.</span> I enjoy when
+              that happens.<br></br><br></br> These are some the things that <span className="glitch">worked.</span>
+            </p>
+          </div>
           <div className="mt-10 w-full h-full">
             <div className="2xl:grid 2xl:grid-cols-3 flex flex-col gap-2 2xl:flex-none font-mono h-fit">
               <div className="opacity-80 overflow-hidden hover:cursor-pointer hover:opacity-100 transition-all duration-1000 relative">
-                <div className="absolute bottom-0 left-0 md:ml-4 mb-3 z-10 text-black"></div>
-                <Image
-                  className="max-h-[200px] rounded-sm object-cover w-full h-full transition-all duration-1000 scale-100"
-                  src={padleClient}
-                  alt="Stuff"
-                ></Image>
+                <a href="https://adorable-concha-1ef0b2.netlify.app/">
+                  <div className="absolute bottom-0 left-0 md:ml-4 mb-3 z-10 text-black"></div>
+                  <Image
+                    className="max-h-[200px] rounded-sm object-cover w-full h-full transition-all duration-1000 scale-100 hover:scale-110"
+                    src={padleClient}
+                    alt="Stuff"
+                  ></Image>
+                </a>
               </div>
               <div className="col-span-2 opacity-80 overflow-hidden hover:cursor-pointer hover:opacity-100 transition-all duration-1000 relative">
-                <Image
-                  className="max-h-[200px] rounded-sm object-cover w-full h-full transition-all duration-1000 scale-100"
-                  src={secondServing}
-                  alt="Stuff"
-                ></Image>
+                <a href="https://bewildered-wig-mite.cyclic.app/">
+                  <Image
+                    className="max-h-[200px] rounded-sm object-cover w-full h-full transition-all duration-1000 scale-100 hover:scale-110"
+                    src={secondServing}
+                    alt="Stuff"
+                  ></Image>
+                </a>
               </div>
               <div className="col-span-2 opacity-80 overflow-hidden hover:cursor-pointer hover:opacity-100 transition-all duration-1000 relative">
-                <Image
-                  className="max-h-[200px] rounded-sm object-cover w-full h-full transition-all duration-1000 scale-100"
-                  src={gogos}
-                  alt="Stuff"
-                ></Image>
+                <a href="https://karlichristensen.github.io/nextjs-gogo-community-kitchen-template-2/">
+                  <Image
+                    className="max-h-[200px] rounded-sm object-cover w-full h-full transition-all duration-1000 scale-100 hover:scale-110"
+                    src={gogos}
+                    alt="Stuff"
+                  ></Image>
+                </a>
               </div>
               <div className="col-span-1 opacity-80 overflow-hidden hover:cursor-pointer hover:opacity-100 transition-all duration-1000 relative">
-                <div className="max-h-[200px] rounded-sm object-cover w-full h-full transition-all duration-1000 scale-100">
-                  <div className="h-[200px] w-full bg-black flex justify-center items-center">
-                    <h1 className="text-white glitch fixed text-center text-6xl">
-                      YOUR PROJECT HERE?
-                    </h1>
+                <a
+                  href="mailto:karlichristensen@post.com?subject=Mail%20from%20Portfolio.com"
+                  className=""
+                >
+                  <div className="max-h-[200px] rounded-sm object-cover w-full h-full transition-all duration-1000 scale-100 hover:scale-110">
+                    <div className="h-[200px] w-full bg-black flex justify-center items-center">
+                      <h1 className="text-white glitch fixed text-center text-6xl hover:hidden">
+                        YOUR PROJECT HERE?
+                      </h1>
+                    </div>
                   </div>
-                </div>
+                </a>
               </div>
             </div>
           </div>
